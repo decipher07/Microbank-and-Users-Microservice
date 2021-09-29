899c9e42a576f3c101dd70233fad4bb55cbd8247
DJ <social.deej@gmail.com>
2021-09-29 13:42:53 +0530

    ✨ feat: users and bank microservice

---
+++ src/routers/bank.routes.ts
@@ -0,0 +1,34 @@
+import { Request, Response, Router } from 'express'
+
+const router : Router = Router();
+
+import bankDatabase from '../database/bankdetails.json'
+
+interface bankInterface {
+    type : string, 
+    balance : number,
+    usersid : number
+}
+
+router.get('/', ( req : Request, res : Response ) : Response => {
+    return res.status(200).send("Blog Service Running Successfully.........")
+})
+
+router.get('/:usersBankId' , ( req : Request, res : Response ) : Response => {
+    let usersBankId : string = req.params.usersBankId ; 
+
+    if ( usersBankId == 'all' )
+        return res.status(200).send(bankDatabase);
+    
+    let database : bankInterface[] = bankDatabase ;
+
+        
+    let blogs : bankInterface[] = database.filter((document) => document.usersid == parseInt(usersBankId) );
+    
+    if ( blogs.length == 0 )
+        return res.status(303).send("No Bank Details for The Given User!");
+
+    return res.status(200).json({ "Message" : "The Below is the Bank Detail of the User ", "blogs" : blogs });
+})
+
+export default router ;
---
+++ src/routers/users.routes.ts
@@ -0,0 +1,67 @@
+import { Request, Response, Router } from 'express'
+import http from 'http'
+
+const router : Router = Router();
+
+import usersDatabase from '../database/users.json'
+
+interface usersInterface {
+    id : number,
+    name : string, 
+    profession : string, 
+    location : string
+}
+
+router.get('/', ( req : Request, res : Response ) : Response => {
+    return res.status(200).send("Users Service Running Successfully.........")
+})
+
+router.get('/:id', ( req: Request, res : Response ) : Response  => {
+    let database : usersInterface[] = usersDatabase ;
+
+    if ( req.params.id == 'all' )
+        return res.send(database);
+
+    let user : usersInterface[] = database.filter((document) => document.id == parseInt(req.params.id))
+    
+    if ( user.length == 0 )
+        return res.status(403).send("Sorry No Details for the User Found");
+    
+    return res.send(user);
+})
+
+router.get('/account-details/:nameOfUser', async ( req : Request , res : Response ) : Promise < void > => {
+    let name : string = req.params.nameOfUser ; 
+
+    try {
+        let database : usersInterface[] = usersDatabase ;
+    
+        let user : usersInterface[] = database.filter((document) => document.name == name)
+    
+        let url : string = `http://localhost:3001/bank/${user[0].id}`;
+        
+        let data = '';
+
+        http.get(url, (resp) => {
+
+                // A chunk of data has been received.
+                resp.on('data', (chunk) => {
+                    data += chunk;
+                });
+
+                // The whole response has been received. Print out the result.
+                resp.on('end', () => {
+                    JSON.parse(data);
+                    res.status(201).send(data);
+                });
+
+            }).on("error", (err) => {
+              console.log("Error: " + err.message);        
+        });
+
+    } catch ( e : any ) {
+        res.send(e);
+    }
+})
+
+export default router ;
