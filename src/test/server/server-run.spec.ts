262fb3cc3af4129ce3c93e399c3b206b41caf126
DJ <social.deej@gmail.com>
2021-09-29 15:13:29 +0530

    ➖ feat: Bank Microservice

--- src/test/server/server-run.spec.ts
+++ src/test/server/server-run.spec.ts
@@ -1,20 +1,8 @@
 import chai from "chai"
 import chaiHtpp from "chai-http"
 import { expect } from "chai"
-import { bankApp, usersApp } from '../../app'
+import { bankApp } from '../../app'
 chai.use(chaiHtpp)
-
-describe("Server Checks for Users Application", () : void => {
-    it("Server Instantiation without Errors", (done) : void => {
-        chai.request(usersApp)
-            .get('/')
-            .end((err : any , res : any ) => {
-                expect(res.status).to.be.equal(200);
-                done();
-            })
-    })
-})
-
 
 describe("Server Checks for Banks Application", () : void => {
     it("Server Instantiation without Errors", (done) : void => {
